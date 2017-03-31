f6507c5bd74d511dc881c68f06e899ced4ff41fc
zgitbox <u.cosmos@yandex.ru>
2017-04-01 00:04:03 +0400

    Final

--- Lesson1/homework1.py
+++ Lesson1/homework1.py
@@ -8,29 +8,28 @@ Homework Lesson1
 4. Записать строку символов в двоичный файл и вывести содержимое файла.
 5. Записать строку символов в файл, явным указанием кодировки latin-1, вывести содержимое файла.
 6. Декодировать содержимое файла из предыдущего задания.
-7. * Определить из jpg файлов был создан ранее всех.
+7. * Определить какой из jpg файлов был создан ранее всех.
 '''
 print('-----1-----')
-open('cat.txt', 'w').write('Записываю строку символов в текстовый файл и вывожу содержимое файла.')
-print(open('cat.txt', 'r').read())
+open('1.txt', 'w').write('Записываю строку символов в текстовый файл и вывожу содержимое файла.')
+print(open('1.txt', 'r').read())
 
 print('\n-----2-----')
-open('cat.txt', 'w', encoding='utf-8').write('Записываю строку символов в файл, c явным указанием кодировки utf-8, и вывожу содержимое файла.')
-print(open('cat.txt', 'r').read())
+open('2.txt', 'w', encoding='utf-8').write('Записываю строку символов в файл, c явным указанием кодировки utf-8, и вывожу содержимое файла.')
+print(open('2.txt', 'r').read())
 
 print('\n-----3-----')
-cat = open('cat.txt', 'rb').read()
+cat = open('2.txt', 'rb').read()
 print(cat.decode())
-# Если открывать в режиме r, то выводится ошибка AttributeError: 'str' object has no attribute 'decode'
-# так и задумано или нужно ещё что-то сделать чтобы он открылся в режиме r?
 
+# Есть очущение, что вторую половину заданий я понял неверно.
 print('\n-----4-----')
-open('cat', 'wb').write(b'I write a line of symbols into binary file and print his content.')
-print(open('cat', 'rb').read())
+open('4', 'wb').write(b'I write a line of symbols into binary file and print his content.')
+print(open('4', 'rb').read())
 
 print('\n-----5-----')
-open('cat', 'w', encoding='latin-1').write('I write a line of symbols into binary file with latin-1 coding and print his content.')
-print(open('cat', 'r').read())
+open('5', 'w', encoding='latin-1').write('I write a line of symbols into binary file with latin-1 coding and print his content.')
+print(open('5', 'r').read())
 
 print('\n-----6-----')
 cat = open('cat', 'rb').read()
